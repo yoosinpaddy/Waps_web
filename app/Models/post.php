405b545e34a365b38f8bf0fbc3acdb1d99a2a36f
Yoosin Paddy <yoosinfrancis@gmail.com>
2021-02-02 13:11:33 +0300

    first commit

---
+++ app/Models/post.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class post extends Model
+{
+    use HasFactory;
+
+    // protected $table = 'posts';
+
+    public function user()
+    {
+        return $this->hasOne('App\User');
+    }
+}
---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_type');//1 admin, 2 Driver, 3 Customer
+            $table->string('first_name');
+            $table->string('surname');
+            $table->string('phone')->unique();
+            $table->string('email')->unique();
+            $table->string('county')->nullable();
+            $table->string('country')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
